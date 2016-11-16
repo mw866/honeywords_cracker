c7148f5f21d37bf796a3b6687330f763d92891be
howard50b <howard50b@gmail.com>
2016-11-15 23:43:33 -0500

    add block.py

--- block.py
+++ block.py
@@ -1,8 +1,29 @@
-#!/bin/env python2.7
+#!/usr/bin/env python2.7
 #howard
 
-def get_block_scores(inputs, m, n):
-	print "Calculating block scores..."
+def decide_func(char):
+	'''Given char and return the corresponding detecting function.'''
+	def isspecial(char):
+		return not str.isalpha(char) and not str.isdigit(char)
+	if str.isalpha(char):
+		return str.isalpha
+	elif str.isdigit(char):
+		return str.isdigit
+	else:
+		return isspecial
 
-	#TODO
-	return [0,0,0,0,0]
+def get_block_scores(sweetwords=['aa123hello#@', 'a1a2a3', 'thisIsCool', '@#!@#haha', 'a!1c#3f%'], m=8, n=5):
+	scores = []
+	for sweetword in sweetwords:
+		block_num = 1
+		func = decide_func(sweetword[0])
+		for i in range(len(sweetword) - 1):
+			if not func(sweetword[i]) == func(sweetword[i + 1]):
+				block_num += 1
+				func = decide_func(sweetword[i + 1])
+		scores.append(block_num)
+	scores = [float(s) / sum(scores) for s in scores]
+	return scores
+
+if __name__ == '__main__':
+	get_block_scores()
