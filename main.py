94572874032dca5d43b873b520496e8fa99bb83a
Sonia Sen <ssen10@email.arizona.edu>
2016-11-16 23:23:07 -0500

    updated to print out the index scores to represent the guess at the true password in sweetword set i

--- main.py
+++ main.py
@@ -6,21 +6,21 @@ def main():
 	print "Usage: python ", sys.argv[0], " m  (no. of sweetword sets/rows), n = 5 (no. of sweetwords/columns) filename\nExample: python main.py 100 5 real_and_sweet.txt "
 	m = int(sys.argv[1])
 	n = int(sys.argv[2])
-	filename = sys.argv[3] 
+	filename = sys.argv[3]
 
 	password_freq_dict = frequency.init_frequency_scores()
 
 	with open(filename) as f:
 		line_n = 1
 		for line in f:
-	
+
 			if line_n >= m: break
 			line_n += 1
 
 			passwords = line.rstrip('\n').split(',')
 			print '\npasswords: ', passwords
 			n = min(len(passwords), n)
-			
+
 			frequency_scores = frequency.get_frequency_scores(password_freq_dict, m, n, passwords)
 			print "frequency_scores:\t", frequency_scores
 			block_scores = block.get_block_scores(passwords, m, n)
@@ -29,7 +29,6 @@ def main():
 			print "dictionary_scores:\t", dictionary_scores
 			get_real(passwords, frequency_scores, block_scores, dictionary_scores)
 
-			 
 	return
 
 def get_real(passwords, frequency_scores, block_scores, dictionary_scores):
@@ -39,9 +38,10 @@ def get_real(passwords, frequency_scores, block_scores, dictionary_scores):
 		password = passwords[i]
 		# [TODO] To fine tune the scoring system.
 		total_score = frequency_scores[i] + block_scores[i] + dictionary_scores[i]
-		total_scores_list.append(total_score)	
+		total_scores_list.append(total_score)
 	max_score = max(total_scores_list)
 	print 'total_scores_list:\t', total_scores_list
+	print 'Best Guess Indices(', max_score, '): ', total_scores_list.index(max_score)
 	print 'Best Guess (', max_score, '): ', passwords[total_scores_list.index(max_score)]
 
 if __name__ == '__main__':
